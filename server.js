df11e95015c27c1912a1a635fbdfb9dc3b965020
Hemanta Rijal <hemantarijal579@gmail.com>
2025-07-12 14:46:51 +0545

    Updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
-const PORT = 3000;
-
 const app = express();
 const connectionDb = require("./config/Connection");
 
@@ -11,28 +8,28 @@ const connectionDb = require("./config/Connection");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Adjust this to your frontend URL
+    origin: "*",
+    // ["http://localhost:5173", "https://your-vercel-app.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
-//routes
+// Routes
 const blogRoutes = require("./routes/blog");
 const contactRoutes = require("./routes/contact");
 
 app.use("/api/blogs", blogRoutes);
 app.use("/api/contact", contactRoutes);
 
+// Start Server
+const PORT = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectionDb(process.env.MONGOOSE_URI);
-    app.listen(PORT, () =>
-      console.log(`Server running: http://localhost:${PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
-    console.error("Error starting the server:", error);
+    console.error("Error starting server:", error);
   }
 };
 
-// Start the server
 start();
